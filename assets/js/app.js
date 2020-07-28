3213592ddc6c05cd30107f40abef5c14e09a65bf
Adnan Niaz <adnanniaz77@yahoo.com>
2020-07-28 15:18:12 -0700

    added helper function to get and update the selected movies prices

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,27 @@ const selectOptions = document.querySelector("#moviesSelector");
 const count = document.querySelector("#count");
 const total = document.querySelector("#total");
 
+let totalPrice = +selectOptions.value;
+
+function updateSelectedSeatsCount() {
+    const selectedSeats = document.querySelectorAll(
+        ".seatingsAndScreen .seat.selected"
+    ).length;
+    count.innerText = selectedSeats;
+    total.innerText = selectedSeats * totalPrice;
+}
+
+selectOptions.addEventListener("change", (e) => {
+    totalPrice = +e.target.value;
+    updateSelectedSeatsCount();
+});
+
 seatingsAndScreen.addEventListener("click", (e) => {
     if (
         e.target.classList.contains("seat") &&
         !e.target.classList.contains("occupied")
     ) {
         e.target.classList.toggle("selected");
+        updateSelectedSeatsCount();
     }
 });
